79ee3357c278852ad7e46fab9e1745928c7fdae6
Sebas Franch <sebasfranch@gmail.com>
2017-05-17 13:14:05 +0200

    Add function printVersion.R file
    File with function printing package name and authorship

---
+++ printVersion/printVersion.R
@@ -0,0 +1,12 @@
+#' Linear model
+#'
+#' This function estimates model parameters in a linear regression model
+#' @param formula formula to be fitted
+#' @param data data
+#' @param ... other arguments
+#' @export
+
+printVersion <- function(){
+  out <- c("Package: printVersion", "\n", "Author: Sebas Franch")
+  cat(out)
+}
